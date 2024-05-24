6d90a458e32cbe6b8cd27a7e427dbf405923e4b5
Joseph Balsavich <balsa013@umn.edu>
2024-05-24 17:57:51 -0500

    added netlify details

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://beta.balsavich.dev/',
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  output: "server",
+  adapter: netlify()
 });
